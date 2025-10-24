a8df652599caf009fd56a24e188b2dc858258bbd
dHung2412 <danghung241204@gmail.com>
2025-10-24 15:14:01 +0700

    Initial commit

--- ingestion/batch_ingestion.py
+++
@@ -1,6 +0,0 @@
-from binance.client import Client
-
-class BinanceBatchIngestion:
-    def __init__(self, api_key: str = None, api_secret: str = None):
-        self.client = Client(api_key, api_secret)
-         
